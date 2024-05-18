39cab1e6af33594ac81dcf33f5ded289ae106b13
Paul Williams <168967091+PaulWSM80@users.noreply.github.com>
2024-05-18 12:29:38 +0100

    feat(Found* JS RPS): add selection action on click
    change player display
    generate cpu display
    
    modified:   01-foundations/03-js-foundations/04-project_rock-paper-scissors/rock-paper-scissors/index.html
    modified:   01-foundations/03-js-foundations/04-project_rock-paper-scissors/rock-paper-scissors/script.js

--- 01-foundations/03-js-foundations/04-project_rock-paper-scissors/rock-paper-scissors/script.js
+++ 01-foundations/03-js-foundations/04-project_rock-paper-scissors/rock-paper-scissors/script.js
@@ -1,11 +1,23 @@
-function rock() {
-    console.log("rock clicked");
+function playerChoice(choice) {
+    document.getElementById("player-choice").innerHTML = choice
+    cpuChoice(choice);
 }
 
-function paper() {
-    console.log("paper clicked");
+function cpuChoice(playerChoice) {
+    let cpuResult = getRandomResult();
+    document.getElementById("cpu-choice").innerHTML = cpuResult;
+    compareResults(playerChoice, cpuResult);
 }
 
-function scissors() {
-    console.log("scissors clicked");
+function getRandomResult() {
+    switch (Math.floor(Math.random() * 3)) {
+        case 0:
+            return "Rock";
+        case 1:
+            return "Paper";
+        case 2:
+            return "Scissors";
+        default:
+            return "ERROR!";
+    };
 }
