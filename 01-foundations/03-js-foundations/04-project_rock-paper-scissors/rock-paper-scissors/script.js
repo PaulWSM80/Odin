89b295ec53e468b07cd685d134ba0816b6d77096
Paul Williams <168967091+PaulWSM80@users.noreply.github.com>
2024-05-18 12:04:08 +0100

    feat(Proj* JS RPS): basic html/css GUI and clickable divs
    modified:   01-foundations/03-js-foundations/04-project_rock-paper-scissors/rock-paper-scissors/index.html
    modified:   01-foundations/03-js-foundations/04-project_rock-paper-scissors/rock-paper-scissors/script.js
    modified:   01-foundations/03-js-foundations/04-project_rock-paper-scissors/rock-paper-scissors/style.css

--- 01-foundations/03-js-foundations/04-project_rock-paper-scissors/rock-paper-scissors/script.js
+++ 01-foundations/03-js-foundations/04-project_rock-paper-scissors/rock-paper-scissors/script.js
@@ -1,1 +1,11 @@
-console.log("Hello, world");
+function rock() {
+    console.log("rock clicked");
+}
+
+function paper() {
+    console.log("paper clicked");
+}
+
+function scissors() {
+    console.log("scissors clicked");
+}
